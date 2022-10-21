a85b433090825dce659ab39399903a6880420ed1
Riju Vijayan <riju.vijayan@digio.com.au>
2022-10-21 13:56:16 +1300

    fix: moved directories

--- src/pages/order/tests/totalUpdates.test.jsx
+++ src/pages/order/tests/totalUpdates.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
-import Options from '../Options';
+import Options from '../../entry/Options';
 
 test('updated scoop subtotal when scoops change', async () => {
   const user = userEvent.setup();
--- src/test-utils/testing-library-utils.jsx
+++ src/test-utils/testing-library-utils.jsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { OrderDetailsProvider } from '../contexts/OrderDetails';
 
+//https://testing-library.com/docs/react-native-testing-library/setup#custom-render
 const renderWithContext = (ui, options) =>
   render(ui, { wrapper: OrderDetailsProvider, ...options });
 
